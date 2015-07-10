f6b033c6151add38ec9b9b4af9cfc3a8ac5bd011
AWboy <baseball_allstar_365@hotmail.com>
2015-07-10 11:05:52 -0600

    bootstrap file and dir

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Use an autoloader!
-require 'config/Bootstrap.php';
+require 'bootstrap/Bootstrap.php';
 require 'config/Controller.php';
 require 'config/Model.php';
 require 'config/View.php';
