775522c143ca155b4a623d79731390a3ce10175b
AWboy <baseball_allstar_365@hotmail.com>
2015-07-10 17:23:05 -0600

    updated bootstrap and index files

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Use an autoloader!
-require 'bootstrap/Bootstrap.php';
+require 'bootstrap/bootstrap.php';
 require 'config/Controller.php';
 require 'config/Model.php';
 require 'config/View.php';
