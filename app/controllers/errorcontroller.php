18f4af4051408ba3510eb9169c568b186f51e1ef
AWboy <baseball_allstar_365@hotmail.com>
2015-07-11 22:33:01 -0600

    turned error messages into messages to views

--- app/controllers/errorcontroller.php
+++ app/controllers/errorcontroller.php
@@ -7,14 +7,16 @@ class Error extends Controller {
 		parent::__construct();
 	}
 	function index(){
+		$this->view->msg = 'Oops there has been an error';
 		$this->view->render('error/index');
-
 	}
 	function  routingerror(){
-		echo "Routing Error";
+		$this->view->msg = 'There appears to be a routing error!';
+		$this->view->render('error/index');
 	}
 	function bigerror(){
-		echo'Oops, Something just went horribley wrong';
+		$this->view->msg = 'There has been a fatal routing error';
+		$this->view->render('error/index');
 	}
 
 }
--- app/views/error/index.php
+++ app/views/error/index.php
@@ -2,6 +2,8 @@
 </head>
 <body>
   <!-- This is the view that will be shown if an error occurs in production mode -->
-Oops! That page is not found
+  <div id="error_message">
+    <?php echo $this->msg; ?>
+  </div>
 </body>
 </html>
